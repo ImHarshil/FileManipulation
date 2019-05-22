c1f757bf6c2bcea3e46fa8eabb5eb965e239e713
Harshil Rathod <50371099+ImHarshil@users.noreply.github.com>
2019-05-22 19:24:27 +0530

    f

--- FileManipulation.py
+++ FileManipulation.py
@@ -1,66 +1,63 @@
 import os
+
+
 class FileManupulation:
 
     def __init__(self):
         pass
 
-
     def __init__(self, filename):
-        self.filename=filename
-        f=self.filename
+        self.filename = filename
+        f = self.filename
        # i=0
        # while(i<len(f)):
         #    if(f[i]=='\\'):
-         #       f[i]='/'
-
-
+        #       f[i]='/'
 
    # def __del__(self):
     #    self.file.close()
 
-    def set_filename(self,filename):
-        self.filename=filename
-        f=self.filename
-        i=0
-        while(i<len(f)):
-            if(f[i]=='\\'):
-                f[i]='/'
-
-
+    def set_filename(self, filename):
+        self.filename = filename
+        f = self.filename
+        i = 0
+        while(i < len(f)):
+            if(f[i] == '\\'):
+                f[i] = '/'
 
     def get_filename(self):
         return self.filename
 
-    def replace_all(self,value,newvalue):
+    def replace_all(self, value, newvalue):
         with open(self.filename) as file:
             content = file.read()
-            content= content.replace(value,newvalue)
+            content = content.replace(value, newvalue)
             del file
-            file=open(self.filename,"w")
+            file = open(self.filename, "w")
             file.write(content)
 
     def show_file(self):
         with open(self.filename)as file:
             return file.read()
 
-    def append_file(self,filename):
-        file1=open(filename)
-        str = file1.read()+"\n"
-        file1=open(self.filename,"a")
+    def append_file(self, filename):
+        file1 = open(filename)
+        str = file1.read() + "\n"
+        file1 = open(self.filename, "a")
         file1.write(str)
         file1.close()
 
     def delete_content(self):
-        file = open(self.filename,"w")
+        file = open(self.filename, "w")
         file.truncate(32)
 
-    def append_content(self,data):
-        file  = open(self.filename,"a")
-        data = "\n"+data
+    def append_content(self, data):
+        file = open(self.filename, "a")
+        data = "\n" + data
         file.write(data)
 
-    def write_content(self,data):
-        file = open(self.filename,"w")
+    def write_content(self, data):
+        file = open(self.filename, "w")
         file.write(data)
 
     @staticmethod
@@ -69,5 +66,4 @@ class FileManupulation:
 
     @classmethod
     def create_file(cls, filename):
-        return open(filename,"w")
-
+        return open(filename, "w")
